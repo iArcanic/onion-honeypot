5fb131f9ac6c556d6f5ed5e8bba3b3c3c0ceae60
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-04-02 19:56:10 +0100

    Added ELK stack containers for logging functionality

--- http-honeypot/src/app.py
+++ http-honeypot/src/app.py
@@ -1,15 +1,39 @@
+import json
+import requests
+
 from flask import Flask, request, jsonify
 
+# Logstash endpoint details
+LOGSTASH_HOST = "logstash"
+LOGSTASH_PORT = 5514
 
 app = Flask(__name__)
 
 
-# Default endpoint
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return "HTTP Honeypot"
+    # Extract request data
+    request_data = {
+        'RemoteAddr': request.remote_addr,
+        'Method': request.method,
+        'RequestURI': request.full_path,
+        'Header': dict(request.headers),
+        'UserAgent': request.headers.get('User-Agent'),
+        'PostForm': request.form,
+    }
+
+    send_log_to_logstash(request_data)
+    return jsonify(request_data)
+
+
+def send_log_to_logstash(data):
+    try:
+        response = requests.post(f"http://{LOGSTASH_HOST}:{LOGSTASH_PORT}", json=data)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+        print("Log sent successfully!")
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending log: {e}")
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-    
