02cdd15606db13af240c81d7c63819280c9b9cc7
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-04-07 18:27:58 +0100

    Added mock user directory and file download functionality

--- http-honeypot/src/routes/directory_listing.py
+++ http-honeypot/src/routes/directory_listing.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file
 from flask_login import login_required, current_user
 
 
@@ -11,19 +11,28 @@ directory_listing_blueprint = Blueprint('directory_listing', __name__, template_
 @login_required
 def directory_listing(username):
     if username == current_user.get_id():
-        user_dir = os.path.join(os.path.abspath('data'), username)
-        entries = os.listdir(user_dir)
+        absolute_path = os.path.join(os.path.abspath('data'), username)
+        entries = os.listdir(absolute_path)
         return render_template('directory_listing.html', username=username, entries=entries)
     else:
         return 'You are not authorized to access this directory.', 403
 
 
-@directory_listing_blueprint.route('/<username>/<folder>', methods=['GET'])
+@directory_listing_blueprint.route('/<username>/<path:requested_path>', methods=['GET'])
 @login_required
-def folder_listing(username, folder):
+def serve_file(username, requested_path):
     if username == current_user.get_id():
-        user_dir = os.path.join(os.path.abspath('data'), username, folder)
-        entries = os.listdir(user_dir)
-        return render_template('directory_listing.html', username=username, folder=folder, entries=entries)
+        # Construct absolute path to the requested resource
+        absolute_path = os.path.join(os.path.abspath('data'), username, requested_path)
+
+        # If it's a directory, list its contents
+        if os.path.isdir(absolute_path):
+            entries = os.listdir(absolute_path)
+            return render_template('directory_listing.html', username=username, folder=requested_path, entries=entries)
+        # If it's a file, serve it for download
+        elif os.path.isfile(absolute_path):
+            return send_file(absolute_path, as_attachment=True)
+        else:
+            return 'Resource not found.', 404
     else:
         return 'You are not authorized to access this directory.', 403
